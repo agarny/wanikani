2d2c3731eff83d2e903bfb8d995fd36935e53759
Alan Garny <agarny@hellix.com>
2016-11-26 22:14:58 +0100

    Make sure that we can build statically on Windows (at least).

--- src/wanikani.cpp
+++ src/wanikani.cpp
@@ -25,6 +25,7 @@ limitations under the License.
 
 //==============================================================================
 
+#include <QDateTime>
 #include <QDesktopWidget>
 #include <QDir>
 #include <QFileInfo>
