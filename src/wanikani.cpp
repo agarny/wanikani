48c779cd91dface2f98ac8dd6bf18302efaa124c
Alan Garny <agarny@hellix.com>
2017-01-13 10:47:01 +0100

    Fixed a small problem with the retrieval of radicals/Kanji/vocabulary for level 10.

--- src/wanikani.cpp
+++ src/wanikani.cpp
@@ -617,15 +617,15 @@ void WaniKani::update()
     QJsonDocument radicalsResponse = (   srsDistributionResponse.isNull()
                                       || srsDistributionResponse.object().contains("error"))?
                                              QJsonDocument():
-                                             waniKaniRequest("radicals/1,2,3,4,5,6,7,8,9,0,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
+                                             waniKaniRequest("radicals/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
     QJsonDocument kanjiResponse = (   radicalsResponse.isNull()
                                    || radicalsResponse.object().contains("error"))?
                                           QJsonDocument():
-                                          waniKaniRequest("kanji/1,2,3,4,5,6,7,8,9,0,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
+                                          waniKaniRequest("kanji/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
     QJsonDocument vocabularyResponse = (   kanjiResponse.isNull()
                                         || kanjiResponse.object().contains("error"))?
                                                QJsonDocument():
-                                               waniKaniRequest("vocabulary/1,2,3,4,5,6,7,8,9,0,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
+                                               waniKaniRequest("vocabulary/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60");
 
     if (   !vocabularyResponse.isNull()
         && !vocabularyResponse.object().contains("error")) {
