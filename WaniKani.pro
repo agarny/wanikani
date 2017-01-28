ce4e40fc8ef1c96cb846d62be03dc5d4e6610870
Alan Garny <agarny@hellix.com>
2017-01-28 22:50:06 +0100

    Added support for zlib.

--- WaniKani.pro
+++ WaniKani.pro
@@ -9,18 +9,45 @@ DEFINES += QT_DEPRECATED_WARNINGS
 win32: LIBS += -lUser32
 mac: LIBS += -framework AppKit
 
-INCLUDEPATH += src/3rdparty/QtSingleApplication
+INCLUDEPATH += src/3rdparty/QtSingleApplication \
+               src/3rdparty/zlib
 
 SOURCES = src/main.cpp \
           src/wanikani.cpp \
           src/widget.cpp \
           src/3rdparty/QtSingleApplication/qtlocalpeer.cpp \
-          src/3rdparty/QtSingleApplication/qtsingleapplication.cpp
+          src/3rdparty/QtSingleApplication/qtsingleapplication.cpp \
+          src/3rdparty/zlib/adler32.c \
+          src/3rdparty/zlib/compress.c \
+          src/3rdparty/zlib/crc32.c \
+          src/3rdparty/zlib/deflate.c \
+          src/3rdparty/zlib/gzclose.c \
+          src/3rdparty/zlib/gzlib.c \
+          src/3rdparty/zlib/gzread.c \
+          src/3rdparty/zlib/gzwrite.c \
+          src/3rdparty/zlib/infback.c \
+          src/3rdparty/zlib/inffast.c \
+          src/3rdparty/zlib/inflate.c \
+          src/3rdparty/zlib/inftrees.c \
+          src/3rdparty/zlib/trees.c \
+          src/3rdparty/zlib/uncompr.c \
+          src/3rdparty/zlib/zutil.c
 
 HEADERS = src/wanikani.h \
           src/widget.h \
           src/3rdparty/QtSingleApplication/qtlocalpeer.h \
-          src/3rdparty/QtSingleApplication/qtsingleapplication.h
+          src/3rdparty/QtSingleApplication/qtsingleapplication.h \
+          src/3rdparty/zlib/zutil.h \
+          src/3rdparty/zlib/crc32.h \
+          src/3rdparty/zlib/deflate.h \
+          src/3rdparty/zlib/gzguts.h \
+          src/3rdparty/zlib/inffast.h \
+          src/3rdparty/zlib/inffixed.h \
+          src/3rdparty/zlib/inflate.h \
+          src/3rdparty/zlib/inftrees.h \
+          src/3rdparty/zlib/trees.h \
+          src/3rdparty/zlib/zconf.h \
+          src/3rdparty/zlib/zlib.h
 
 FORMS = src/widget.ui
 
---
+++ src/3rdparty/zlib/zlibplugin.cpp
@@ -0,0 +1,51 @@
+/*******************************************************************************
+
+Copyright The University of Auckland
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******************************************************************************/
+
+//==============================================================================
+// zlib plugin
+//==============================================================================
+
+#include "zlibplugin.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace zlib {
+
+//==============================================================================
+
+PLUGININFO_FUNC zlibPluginInfo()
+{
+    Descriptions descriptions;
+
+    descriptions.insert("en", QString::fromUtf8("a plugin to access <a href=\"http://www.zlib.net/\">zlib</a>."));
+    descriptions.insert("fr", QString::fromUtf8("une extension pour accéder <a href=\"http://www.zlib.net/\">zlib</a>."));
+
+    return new PluginInfo(PluginInfo::ThirdParty, false, false,
+                          QStringList(),
+                          descriptions);
+}
+
+//==============================================================================
+
+}   // namespace zlib
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
