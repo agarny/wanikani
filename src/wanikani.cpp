abd3795c76d7b0a9e581ae1958775e71f088585c
Alan Garny <agarny@hellix.com>
2019-09-08 16:28:32 +1200

    Improved our retrieval of WaniKani data.

--- src/wanikani.cpp
+++ src/wanikani.cpp
@@ -874,7 +874,8 @@ void WaniKani::checkNbOfReplies()
     ++mNbOfReplies;
 
     if (mNbOfReplies == mNbOfNeededReplies) {
-        if (   validJsonDocument(mStudyQueueResponse)
+        if (   validJsonDocument(mUserResponse)
+            || validJsonDocument(mStudyQueueResponse)
             || validJsonDocument(mLevelProgressionResponse)
             || validJsonDocument(mSrsDistributionResponse)
             || validJsonDocument(mRadicalsResponse)
@@ -897,7 +898,10 @@ void WaniKani::doUpdate(bool pForce)
 {
     // Make sure that we have an API key
 
-    if (mApiKey.isEmpty() || mApiToken.isEmpty()) {
+    bool hasApiKey = !mApiKey.isEmpty();
+    bool hasApiToken = !mApiToken.isEmpty();
+
+    if (!hasApiKey && !hasApiToken) {
         emit error();
 
         return;
@@ -910,28 +914,31 @@ void WaniKani::doUpdate(bool pForce)
     //  - the user's list of Kanji (and their information)
     //  - the user's list of vocabulary (and their information)
 
+    bool retrieveData = !mApiKey.isEmpty();
+    bool retrieveV2Data = !mApiToken.isEmpty() && (pForce || !mUser.mHasData);
+
     mNbOfReplies = 0;
-    mNbOfNeededReplies = 6;
+    mNbOfNeededReplies = 6*int(retrieveData)+int(retrieveV2Data);
 
-    if (pForce || !mUser.mHasData) {
-        ++mNbOfNeededReplies;
-
+    if (retrieveV2Data) {
         QObject::connect(waniKaniV2NetworkReply("user"), &QNetworkReply::finished,
                          this, &WaniKani::userReply);
     }
 
-    QObject::connect(waniKaniNetworkReply("study-queue"), &QNetworkReply::finished,
-                     this, &WaniKani::studyQueueReply);
-    QObject::connect(waniKaniNetworkReply("level-progression"), &QNetworkReply::finished,
-                     this, &WaniKani::levelProgressionReply);
-    QObject::connect(waniKaniNetworkReply("srs-distribution"), &QNetworkReply::finished,
-                     this, &WaniKani::srsDistributionReply);
-    QObject::connect(waniKaniNetworkReply("radicals/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
-                     this, &WaniKani::radicalsReply);
-    QObject::connect(waniKaniNetworkReply("kanji/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
-                     this, &WaniKani::kanjiReply);
-    QObject::connect(waniKaniNetworkReply("vocabulary/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
-                     this, &WaniKani::vocabularyReply);
+    if (retrieveData) {
+        QObject::connect(waniKaniNetworkReply("study-queue"), &QNetworkReply::finished,
+                         this, &WaniKani::studyQueueReply);
+        QObject::connect(waniKaniNetworkReply("level-progression"), &QNetworkReply::finished,
+                         this, &WaniKani::levelProgressionReply);
+        QObject::connect(waniKaniNetworkReply("srs-distribution"), &QNetworkReply::finished,
+                         this, &WaniKani::srsDistributionReply);
+        QObject::connect(waniKaniNetworkReply("radicals/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
+                         this, &WaniKani::radicalsReply);
+        QObject::connect(waniKaniNetworkReply("kanji/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
+                         this, &WaniKani::kanjiReply);
+        QObject::connect(waniKaniNetworkReply("vocabulary/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60"), &QNetworkReply::finished,
+                         this, &WaniKani::vocabularyReply);
+    }
 }
 
 //==============================================================================
